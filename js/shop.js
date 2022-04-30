8e128b9b7c95630083f7187c03bcd46d994693f6
Fan <1259075289@qq.com>
2022-05-01 00:11:07 +0800

    项目的提交

--- js/shop.js
+++ js/shop.js
@@ -14,25 +14,60 @@ class shop {
   async getData() {
     let { data, status } = await axios.get(this.baseUrl);
     if (status != 200) throw new error("请求失败");
-    let ul = "";
-    data.forEach((ele) => {
-      ul += `
-              <li>
-                <img src="${ele.img}" alt="" />
-                <p>${ele.name}</p>
-                <p>${ele.price}</p>
-              </li>
-           `;
-      rightObj.innerHTML = ul;
-    });
+
+    // 懒加载
+
+    // 设置页数和每页显示多少个
+    let page = 1;
+    let pageSize = 12;
+
+    let itemsObj = [];
+    
+    // 取出数据追加到页面中
+    function render(curPage) {
+      // 计算开始位置和结束位置
+      let startPage = (curPage - 1) * pageSize;
+      let endPage = startPage + pageSize;
+     
+      setTimeout(() => {
+        let showData = data.slice(startPage, endPage);
+        console.log(showData);
+        let ul = "";
+        showData.forEach((ele) => {
+          ul += `
+          <li>
+          <img src="${ele.img}" alt="" />
+          <p>${ele.name}</p>
+          <p>${ele.price}</p>
+          </li>
+          `;
+        });
+        rightObj.innerHTML += ul;
+        itemsObj = Array.from(rightObj.children);
+      }, 700);
+    }
+    
+    render(page);
+
+    // 可视区高度
+    let cliH = document.documentElement.clientHeight;
+    window.onscroll = function(){
+      // 滚动条高度
+      let sTop = document.documentElement.scrollTop;
+      // 内容高度
+      let contH = itemsObj[itemsObj.length-1].offsetTop;
+      console.log("可视区"+cliH+"滚动条"+sTop+"内容"+contH);
+      
+      if((cliH + sTop) > contH){
+        render(++page);
+      }
+    }
 
     // 点击按钮切换样式
     arr.forEach((item, index) => {
       item.onclick = function () {
         if (this.className == "nocheck") {
           this.className = "check";
-
-          console.log(index);
           if (index == 0) {
             let car718 = "";
             for (let i = 0; i <= 9; i++) {
@@ -75,7 +110,7 @@ class shop {
           if (index == 3) {
             let panamera = "";
             for (let i = 27; i <= 47; i++) {
-                panamera += `
+              panamera += `
                             <li>
                               <img src="${data[i].img}" alt="" />
                               <p>${data[i].name}</p>
@@ -88,7 +123,7 @@ class shop {
           if (index == 4) {
             let Macan = "";
             for (let i = 48; i <= 51; i++) {
-                Macan += `
+              Macan += `
                             <li>
                               <img src="${data[i].img}" alt="" />
                               <p>${data[i].name}</p>
@@ -101,7 +136,7 @@ class shop {
           if (index == 5) {
             let Cayenne = "";
             for (let i = 52; i <= 66; i++) {
-                Cayenne += `
+              Cayenne += `
                             <li>
                               <img src="${data[i].img}" alt="" />
                               <p>${data[i].name}</p>
@@ -116,13 +151,7 @@ class shop {
           rightObj.innerHTML = ul;
         }
       };
-      anniu.onclick = function () {
-        for(let i = 0; i<arr.length; i++){
-          arr[i].className = "nocheck";
-      }}
     });
-
-   
   }
 }
 new shop();
